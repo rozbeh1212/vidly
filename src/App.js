4be39f7cc4e33448ece20a9b9c319b1e758122b6
ASUS <eb.mehrdad@gmail.com>
2021-05-10 19:16:10 +0430

    mosh hamedai movie added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// eslint-disable-next-line no-unused-vars
+import  React, {Component} from 'react';
 import './App.css';
+// eslint-disable-next-line no-unused-vars
+import { render } from 'react-dom';
 
-function App() {
+
+
+
+class App extends Comment {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+<main className="container">
+
+
+
+
+</main>
+
+
   );
+
+    
+  }
 }
 
 export default App;
